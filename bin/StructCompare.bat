883b6174fffbd196d34e65dca40a140f4165f93c
Wei Cao <davecao@bi.a.u-tokyo.ac.jp>
2016-05-20 14:20:03 +0900

    Add start scripts for windows and *unix

--- bin/StructCompare.bat
+++ bin/StructCompare.bat
@@ -2,7 +2,7 @@ REM A batch script to call java bilab-structure-1.0-jar-with-dependencies.jar
 
 @echo off
 (
-set JAVA_PATH=""C:\Program Files\Java\jdk1.7.0_17\bin";"
+set JAVA_PATH=""C:\Program Files (x86)\Java\jdk1.8.0_91\bin"
 set PATH=%PATH%;%JAVA_PATH%
 
 set @FWDIR=%CD%
